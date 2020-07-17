645fbb44c3072909d65afbc3f1f04a7780ab5d56
Aaron Gesmer <ajgesmer@comcast.net>
2020-07-17 15:16:27 -0400

    displays result's title, year, and poster

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import './App.css';
 
 export default function App() {
+  // state:
   const [queriedItem, setQueriedItem] = useState(null);
   const [query, setQuery] = useState('');
 
+  // event handlers:
   const onInputChange = (query) => {
     setQuery(query.target.value);
   };
@@ -13,20 +15,31 @@ export default function App() {
     fetch(`https://www.omdbapi.com/?apikey=c9e12726&t=${query}`)
     .then(res => res.json())
     .then(result => {
-      console.log('hmmm', result);
       setQueriedItem(JSON.stringify(result));
     });
+  };
+
+  // helper methods:
+  const getResults = () => {
+    const result = JSON.parse(queriedItem);
+    return (
+      <div>
+        <h1>{result.Title} ({result.Year})</h1>
+        <img src={result.Poster} />
+      </div>
+    );
   };
 
   return (
     <div className="root">
       <div className="searchBox">
-        <input type="text" placeholder="Search for a title..." value={query} onInput={onInputChange.bind(this)} />
+        <input type="text" placeholder="Search for a title..." value={query} onChange={onInputChange.bind(this)} />
         <button onClick={onSearchClick.bind(this)}>Search</button>
       </div>
-      <div>
+      <hr />
+      <div className="searchResults">
         <h1>Your results:</h1>
-        {queriedItem}
+        {queriedItem == null ? null : getResults()}
       </div>
     </div>
   );
